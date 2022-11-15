afe97af3baeec3020ca4d9cb01ba5635c81b00d3
SebastianCM <sebastiancmarquez1998@gmail.com>
2022-11-15 17:56:39 -0500

    v1

---
+++ main.js
@@ -0,0 +1,20 @@
+const ShowSocial = (toggleCard, socialCard) => {
+    const toggle = document.getElementById(toggleCard),
+            social = document.getElementById(socialCard)
+
+            toggle.addEventListener('click', () => {
+                // If the animation class exists, we add the down-animation class
+                if(social.classList.contains('animation')){
+                    social.classList.add('down-animation')
+
+                    //we remove the down-animation class
+                    setTimeout(() =>{
+                        social.classList.remove('down-animation')
+                    }, 1000)
+                }
+
+                // We add the animation class to the div tag with the card__social class
+                social.classList.toggle('animation')
+            })
+}
+ShowSocial('card-toggle', 'card-social')
